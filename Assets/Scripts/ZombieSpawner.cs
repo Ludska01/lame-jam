c83e85a0aa82d96f1b353053ebc744816d8de481
Dragan <gagzo78@gmail.com>
2022-12-03 12:39:40 +0100

    mostly finished

--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -5,29 +5,49 @@ using UnityEngine;
 public class ZombieSpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject zombie;
+    private List<GameObject> zombies;
     [SerializeField]
-    private GameObject big_zombie;
+    private List<GameObject> big_zombies;
 
-    private float zombie_interval = 5f;
-
-    private float big_zombie_interval = 10f;
+    [SerializeField]
+    private float zombieSpawnRate = 5f;
+    [SerializeField]
+    private float bigZombieSpawnRate= 10f;
+    private float gameTime;
+    [SerializeField]
+    private float timeToWait = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawn_enemy(zombie_interval,zombie));
-        StartCoroutine(spawn_enemy(big_zombie_interval,big_zombie));
+        StartCoroutine(CSpawnZombies(timeToWait));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameTime += Time.deltaTime;
     }
 
-    private IEnumerator spawn_enemy(float interval, GameObject zombie) {
-        yield return new WaitForSeconds(interval);
-        GameObject new_zombie = Instantiate(zombie, new Vector3(Random.Range(-5f,5), Random.Range(-5f,5), 0), Quaternion.identity);
-        StartCoroutine(spawn_enemy(interval,zombie));
+    void SpawnZombies(int quantityToSpawnZombie, int quantityToSpawnBigZombie){
+        for (int i = 0; i < quantityToSpawnZombie; i++)
+        {
+            int rand = Random.Range(0, zombies.Count);
+            Instantiate(zombies[rand], transform.position, Quaternion.identity);
+        }
+
+        for (int i = 0; i < quantityToSpawnBigZombie; i++)
+        {
+            int rand = Random.Range(0, big_zombies.Count);
+            Instantiate(big_zombies[rand], transform.position, Quaternion.identity);
+        }
+        StartCoroutine(CSpawnZombies(timeToWait));
+    }   
+
+    IEnumerator CSpawnZombies(float timeToWait){
+        yield return new WaitForSeconds(timeToWait);
+        int quantityToSpawnZombie = (int) (gameTime / zombieSpawnRate);
+        int quantityToSpawnBigZombie = (int) (gameTime / bigZombieSpawnRate);
+        SpawnZombies(quantityToSpawnZombie, quantityToSpawnBigZombie);
     }
+    
 }
